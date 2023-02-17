7e9e058a1b53a4f2593bcce5240b002dc3542d31
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-02-17 20:52:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,35 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for(unit64_t i=2; i<sqrt(value); i++)
+    if(value%i==0)
+      return false;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  unit64_t count = 0, value=1;
+  while(count < n){
+    value++;
+    if(checkPrime(value))
+      count ++;
+  }
+  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    while(){
+      value++;
+      if(checkPrime(value))
+        break;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  unit64_t summ=0;
+  for(unit65_t j=2; j<hbound; j++)
+    if(checkPrime(j))
+      summ += i;
+  return summ;
 }
